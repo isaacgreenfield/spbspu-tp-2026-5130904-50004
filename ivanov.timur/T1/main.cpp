a1ce7a5a01a08c6f5bd986494116e5eeeb28b9d8
isaacgreenfield <ivtim708@gmail.com>
2026-05-13 22:50:15 +0300

    added basic ioguard

--- ivanov.timur/T1/main.cpp
+++ ivanov.timur/T1/main.cpp
@@ -20,7 +20,25 @@ namespace ivanov {
     std::pair<long long, unsigned long long>& ref;
   };
 
+  class IOGuard {
+  public:
+    explicit IOGuard(std::basic_ios<char>& s):
+      s_(s), width_(s.width()), fill_(s.fill()), prc_(s.precision()), fmt_(s.flags()) {}
 
+    ~IOGuard() {
+      s_.width(width_);
+      s_.fill(fill_);
+      s_.precision(prc_);
+      s_.flags()
+    }
+
+  private:
+    std::basic_ios<char>& s_;
+    std::streamsize width_;
+    std::streamsize prc_;
+    std::basic_ios<char>::fmtflags fmt_;
+    char fill_;
+  };
 }
 
 int main() {

579b7b6b8e26934033167de6501c5acfd10b25bb
isaacgreenfield <ivtim708@gmail.com>
2026-05-13 22:40:52 +0300

    DataStruct added

--- ivanov.timur/T1/main.cpp
+++ ivanov.timur/T1/main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
+namespace ivanov {
+  struct DataStruct {
+    long long key1;
+    std::pair<long long, unsigned long long> key2;
+    std::string key3;
+  };
+}
+
 int main() {
   return 0;
 }

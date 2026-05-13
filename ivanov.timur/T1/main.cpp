f81a636d134d3acaa8c73c7234e4ff11637ed084
isaacgreenfield <ivtim708@gmail.com>
2026-05-13 23:16:58 +0300

    added full normal input

--- ivanov.timur/T1/main.cpp
+++ ivanov.timur/T1/main.cpp
@@ -10,20 +10,24 @@ namespace ivanov {
   struct DelimiterIO {
     char exp;
   };
+
   struct StringIO {
-    std::string& ref;
+    std::string &ref;
   };
+
   struct LongIO {
-    long long& ref;
+    long long &ref;
   };
+
   struct PairIO {
-    std::pair<long long, unsigned long long>& ref;
+    std::pair<long long, unsigned long long> &ref;
   };
 
   class IOGuard {
   public:
-    explicit IOGuard(std::basic_ios<char>& s):
-      s_(s), width_(s.width()), fill_(s.fill()), prc_(s.precision()), fmt_(s.flags()) {}
+    explicit IOGuard(std::basic_ios<char> &s): s_(s), width_(s.width()), fill_(s.fill()), prc_(s.precision()),
+                                               fmt_(s.flags()) {
+    }
 
     ~IOGuard() {
       s_.width(width_);
@@ -33,14 +37,14 @@ namespace ivanov {
     }
 
   private:
-    std::basic_ios<char>& s_;
+    std::basic_ios<char> &s_;
     std::streamsize width_;
     std::streamsize prc_;
     std::basic_ios<char>::fmtflags fmt_;
     char fill_;
   };
 
-  std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+  std::istream &operator>>(std::istream &in, DelimiterIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
     char c = 0;
@@ -48,7 +52,8 @@ namespace ivanov {
     if (in && c != dest.exp) in.setstate(std::ios::failbit);
     return in;
   }
-  std::istream& operator>>(std::istream& in, StringIO&& dest) {
+
+  std::istream &operator>>(std::istream &in, StringIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
     char q;
@@ -63,7 +68,8 @@ namespace ivanov {
     dest.ref = std::move(str);
     return in;
   }
-  std::istream operator>>(std::istream& in, LongIO&& dest) {
+
+  std::istream operator>>(std::istream &in, LongIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
     long long val;
@@ -78,10 +84,12 @@ namespace ivanov {
     dest.ref = val;
     return in;
   }
-  std::istream operator>>(std::istream& in, PairIO&& dest) {
+
+  std::istream operator>>(std::istream &in, PairIO &&dest) {
     std::istream::sentry sentry(in);
     if (!sentry) return in;
     char c;
+
     in >> c;
     if (!in || c != '(') {
       in.setstate(std::ios::failbit);
@@ -116,6 +124,66 @@ namespace ivanov {
 
     dest.ref.first = n;
     dest.ref.second = d;
+    return in;
+  }
+
+  std::istream &operator=(std::istream &in, DataStruct &dest) {
+    std::istream::sentry sentry(in);
+    if (!sentry) return in;
+
+    DataStruct input;
+    bool hasKey1 = false, hasKey2 = false, hasKey3 = false;
+
+    if (!(in >> DelimiterIO{'('})) return in;
+
+    while (true) {
+      in >> std::ws;
+      if (!in) break;
+
+      if (in.peek() == ')') {
+        in.get();
+        break;
+      }
+      if (!(in >> DelimiterIO{':'})) break;
+
+      std::string key;
+      if (!(in >> key)) break;
+      in >> std::ws;
+
+      if (key == "key1") {
+        if (hasKey1) {
+          in.setstate(std::ios::failbit);
+          break;
+        }
+        LongIO llio{input.key1};
+        if (!(in >> std::move(llio))) break;
+        hasKey1 = true;
+      } else if (key == "key2") {
+        if (hasKey2) {
+          in.setstate(std::ios::failbit);
+          break;
+        }
+        PairIO pio{input.key2};
+        if (!(in >> std::move(pio))) break;
+        hasKey2 = true;
+      } else if (key == "key3") {
+        if (hasKey3) {
+          in.setstate(std::ios::failbit);
+          break;
+        }
+        StringIO sio{input.key3};
+        if (!(in >> std::move(sio))) break;
+        hasKey3 = true;
+      } else {
+        in.setstate(std::ios::failbit);
+        break;
+      }
+    }
+    if (!in || !(hasKey1 && hasKey2 && hasKey3)) {
+      in.setstate(std::ios::failbit);
+      return in;
+    }
+    dest = std::move(input);
     return in;
   }
 }

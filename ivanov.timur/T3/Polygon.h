786eadd2d05e31a2408561e7d4207209cb285b4a
isaacgreenfield <ivtim708@gmail.com>
2026-05-16 14:49:24 +0300

    Added polygon and point

---
+++ ivanov.timur/T3/Polygon.h
@@ -0,0 +1,12 @@
+#include <vector>
+#include "Point.h"
+
+#ifndef POLYGON_H
+#define POLYGON_H
+using namespace ivanov;
+namespace ivanov {
+  struct Polygon {
+    std::vector<Point> points;
+  };
+}
+#endif
--- ivanov.timur/T3/main.cpp
+++ ivanov.timur/T3/main.cpp
@@ -1,3 +1,13 @@
-int main() {
+#include <iostream>
+#include "Polygon.h"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    std::cerr << "Bad input error\n";
+    return 1;
+  }
+
+  std::pmr::vector<Polygon> data;
+
   return 0;
 }

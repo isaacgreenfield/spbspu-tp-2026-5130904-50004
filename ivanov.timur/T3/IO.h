a325c1a168a0a835ec4848aa1e059d54838ae6ce
isaacgreenfield <ivtim708@gmail.com>
2026-05-16 14:55:06 +0300

    copied and swaped operator>> for basic classes

--- ivanov.timur/T3/IO.h
+++ ivanov.timur/T3/IO.h
@@ -2,6 +2,7 @@
 #define IO_H
 #include "Polygon.h"
 #include <ios>
+#include <istream>
 
 using namespace ivanov;
 namespace ivanov {
@@ -35,7 +36,51 @@ private:
   char fill_;
 };
 
+inline std::istream& operator>>(std::istream& in, DelimiterIO&& dest) {
+  std::istream::sentry sentry(in);
+  if (!sentry) return in;
+  char c = 0;
+  in >> c;
+  if (in && c != dest.exp) in.setstate(std::ios::failbit);
+  return in;
+}
 
+inline std::istream& operator>>(std::istream& in, PointIO&& dest) {
+  std::istream::sentry sentry(in);
+  if (!sentry) return in;
+
+  Point temp;
+  in >> DelimiterIO{'('} >> temp.x >> DelimiterIO{';'} >> temp.y >> DelimiterIO{')'};
+  if (in) {
+    dest.ref = temp;
+  }
+  return in;
+}
+
+inline std::istream& operator>>(std::istream& in, Polygon& dest) {
+  std::istream::sentry sentry(in);
+  if (!sentry) return in;
+
+  int n = 0;
+  if (!(in >> n) || n <= 0) {
+    in.setstate(std::ios::failbit);
+    return in;
+  }
+
+  std::vector<Point> points;
+  points.reserve(n);
+  for (int i = 0; i < n; ++i) {
+    Point p;
+    PointIO pio{p};
+    if (!(in >> std::move(pio))) {
+      return in;
+    }
+    points.push_back(p);
+  }
+
+  dest.points = std::move(points);
+  return in;
+}
 
 }
 
